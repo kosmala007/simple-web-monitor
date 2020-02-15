0ddd49328c581c3a56071c3fe2ae0fee101552fe
Marcin Kosmala <kosmala007@gmail.com>
2020-02-15 18:52:49 +0100

    Agregate statements in var

--- swm.sh
+++ swm.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 domain='https://admin.com/'
+statements=''
 
 ## check website http status
 httpResponse=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $domain)
 httpStatus=$(echo $httpResponse | tr -d '\n' | sed -E 's/.*HTTPSTATUS:([0-9]{3})$/\1/')
 if [ $httpStatus -ne "200" ] ; then
+    statements+=$'Critical https status code: '$httpStatus$'\n\n'
     echo "Critical https status code: $httpStatus"
 fi
 
 ## check website response size
 responseSize=$(echo $httpResponse | wc -c)
 if [ $responseSize -lt 512 ] ; then
+    statements+=$'Critical website size: '$(echo $responseSize | numfmt --to=si)$'\n\n'
     echo "Critical website size: "$(echo $responseSize | numfmt --to=si)
 fi
